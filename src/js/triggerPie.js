aa5a257a2fceda4a36321669119e7bb848f2a9a3
Hiroshi Muraoka <h.muraoka714@gmail.com>
2018-12-04 06:01:09 +0900

    add trigerPie.js

---
+++ src/js/triggerPie.js
@@ -0,0 +1,15 @@
+module.exports = function() {
+  var carousel = document.querySelector('.carousel');
+  var slides = document.querySelectorAll('.carousel__slide');
+  function triggerAnimate() {
+    for (var i = 0; i < slides.length; i++) {
+      if (slides[i].classList.contains('carousel__slide--active')) {
+        var pie = slides[i].querySelector('.pie');
+        console.log(pie);
+        pie.setAttribute('data-animated', '');
+      }
+    }
+  }
+  triggerAnimate();
+  carousel.addEventListener('slidemove', triggerAnimate);
+};
