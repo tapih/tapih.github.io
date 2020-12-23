072e6863f9594b8c9b47598adbc835d4f3db2021
Hiroshi Muraoka <h.muraoka714@gmail.com>
2020-12-24 01:53:57 +0900

    Setup eslint
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,48 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
+  mode: "development",
+  entry: "./src/js/index.js",
   output: {
-    filename: 'js/script.js',
-    path: path.join(__dirname, 'dist')
+    filename: "js/script.js",
+    path: path.join(__dirname, "dist")
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist')
+    contentBase: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
-          'css-loader',
+          "style-loader",
+          "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [require('postcss-nested')]
+                plugins: [require("postcss-nested")]
               }
             }
           },
-          'sass-loader'
+          "sass-loader"
         ]
       },
       {
         test: /\.(jpg|svg)$/,
-        use: ['file-loader']
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "css/img",
+          publicPath: "css/img"
+        }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: "src/index.html"
     })
   ]
 };
